5e49a7c19cc8a9d6c48503aa1cb04281accf7d0d
GPast-Studio <gdeenk@gmail.com>
2022-09-28 05:06:43 +0900

    Initial commit

---
+++ app/src/main/java/com/gpaststudio/signuppractice_gpstudio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gpaststudio.signuppractice_gpstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
